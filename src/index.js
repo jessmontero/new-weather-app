67737d7ccb1cb84fc87039ae04e746222bc72513
Jess Montero <87496543+jessmontero@users.noreply.github.com>
2021-07-24 13:48:05 +0100

    Added JS for city search, temp/stats, day/time

---
+++ src/index.js
@@ -0,0 +1,74 @@
+let currentDayTime = document.querySelector("#day-time");
+
+let date = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[date.getDay()];
+
+let hours = ("0" + date.getHours()).slice(-2);
+let minutes = ("0" + date.getMinutes()).slice(-2);
+
+let dayTime = `${day} ${hours}:${minutes}`;
+
+currentDayTime.innerHTML = dayTime;
+
+//
+
+function showTemperature(response) {
+  document.querySelector("#search-input").value = "";
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#conditions").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function search(city) {
+  let apiKey = "ddf0440bcec2a49b426ccbeada3e4574";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  search(city);
+}
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "ddf0440bcec2a49b426ccbeada3e4574";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentPosition() {
+  document.querySelector("#search-input").value = "";
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let form = document.querySelector("#city-search");
+form.addEventListener("submit", handleSubmit);
+
+let button = document.querySelector("#current-location-button");
+button.addEventListener("click", getCurrentPosition);
+
+search("Miami");
